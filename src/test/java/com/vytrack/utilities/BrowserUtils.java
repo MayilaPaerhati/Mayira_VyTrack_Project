cfde9506f039e9073e42fb166a02b73453209ab2
Mayira <mahira223@hotmail.com>
2022-10-13 00:02:52 -0400

    BrowserUtils updated

--- src/test/java/com/vytrack/utilities/BrowserUtils.java
+++ src/test/java/com/vytrack/utilities/BrowserUtils.java
@@ -1,6 +1,8 @@
 package com.vytrack.utilities;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 public class BrowserUtils {
@@ -19,6 +21,21 @@ public class BrowserUtils {
     public static void verifyTitle(WebDriver driver, String expectedTitle) {
         String actualTile = driver.getTitle();
         Assert.assertEquals(actualTile, expectedTitle);
+    }
+
+
+    public static void login(WebDriver driver, String username){
+        driver.get("https://qa2.vytrack.com/user/login");
+
+        WebElement inputUsername = driver.findElement(By.id("prependedInput"));
+        inputUsername.sendKeys(username);
+
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@type='password']"));
+        inputPassword.sendKeys("UserUser123");
+
+        WebElement loginBtn = driver.findElement(By.xpath("//button[.='Log in']"));
+        loginBtn.click();
+
     }
 
 }
